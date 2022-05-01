538fe3f2cc6d36e20e4c0ce78c1716be2a8b7354
mnzvr <mansurjah@gmail.com>
2022-05-01 16:42:30 -0400

    pages update

---
+++ client/src/pages/SignUp/Signup.js
@@ -0,0 +1,70 @@
+import { useRef, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+import "./SignUp.scss"
+import { axiosInstance } from "../../config";
+
+const SignUp = () => {
+
+    const userRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const [errors, setErrors] = useState(null)
+
+    const history = useHistory();
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+        setErrors(null)
+
+        const enteredName = userRef.current.value;
+        const enteredEmail = emailRef.current.value;
+        const enteredPassword = passwordRef.current.value;
+
+        try{
+           const res =  await axiosInstance.post("auth/register", {
+                username: enteredName,
+                email: enteredEmail,
+                password: enteredPassword
+            })
+            console.log(res);
+            res.data && history.replace("/signin")
+        } catch (err) {
+            console.log(err.response.data.msg)
+            const msg = err.response.data.msg
+            setErrors(msg)
+        }
+
+    }
+
+    return (
+        <div className="signupPage">
+            <div className="signup">
+
+
+                <div className="signup1">
+
+                    <h3 className="signup1__text">Welcome to TattleGo</h3>
+                    <p className="signup1__para">Convos on the Go.</p>
+                </div>
+
+                <div className="signup2">
+                <form onSubmit={submitHandler}> 
+                    <h3 className="signup2__text">Sign Up</h3>
+                    {errors && <p className="signup2__error">{errors}</p>}
+                    <input ref={userRef} className="signup2__box" type="text" required placeholder="Username" />
+                    <input ref={emailRef} className="signup2__box" type="text" required placeholder="Email" />
+                    <input min="8" max="20" ref={passwordRef} className="signup2__box" type="password" required placeholder="Password" />
+                    <button className="signup2__submit">Sign Up</button>
+                    </form>
+                    <p className="signup2__para">Already Registered? <Link to="/login">Log in now</Link></p>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default SignUp;
